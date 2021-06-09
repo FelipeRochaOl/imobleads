b7139739ca9860ca7c7ffb6f372d5c8e827de326
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 04:49:59 -0300

    Named route

--- app/Modules/Health/Routes/index.ts
+++ app/Modules/Health/Routes/index.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('health', 'HealthController.index').prefix('api')
+Route.get('health', 'HealthController.index').prefix('api').as('health.index')
 
 export default Route
