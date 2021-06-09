d7103009d78fd80e124d1fbcf8a8da97cfb09157
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 03:58:51 -0300

    add new routes in app

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-import 'App/Modules/Health/routes'
+import 'App/Modules/Health/Routes'
+import 'App/Modules/Session/Routes'
 
 Route.get('/', async () => {
   return { hello: 'world' }
