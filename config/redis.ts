89a75d1cbe3c59097dd26bfdc03149a0d9e97008
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 02:09:41 -0300

    Add redis

---
+++ config/redis.ts
@@ -0,0 +1,24 @@
+import Env from '@ioc:Adonis/Core/Env'
+import { RedisConfig } from '@ioc:Adonis/Addons/Redis'
+
+const redisConfig: RedisConfig = {
+  connection: Env.get('REDIS_CONNECTION'),
+  connections: {
+    local: {
+      host: Env.get('REDIS_HOST'),
+      port: Env.get('REDIS_PORT'),
+      password: Env.get('REDIS_PASSWORD', ''),
+      db: 0,
+      keyPrefix: 'imobleads_',
+    },
+    session: {
+      host: Env.get('REDIS_HOST'),
+      port: Env.get('REDIS_PORT'),
+      password: Env.get('REDIS_PASSWORD', ''),
+      db: 1,
+      keyPrefix: 'session_imobleads_',
+    },
+  },
+}
+
+export default redisConfig
