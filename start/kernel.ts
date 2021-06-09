213084b54ff1f16438ec8d2c3470a91cb8765f82
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 20:46:46 -0300

    update path

--- start/kernel.ts
+++ start/kernel.ts
@@ -1,6 +1,6 @@
 import Server from '@ioc:Adonis/Core/Server'
 Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
 Server.middleware.registerNamed({
-  auth: () => import('App/Modules/Session/Middleware/Auth'),
-  silentAuth: () => import('App/Modules/Session/Middleware/SilentAuth'),
+  auth: () => import('App/Modules/Session/Middlewares/Auth'),
+  silentAuth: () => import('App/Modules/Session/Middlewares/SilentAuth'),
 })
