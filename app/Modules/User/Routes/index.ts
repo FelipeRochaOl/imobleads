c55b599783c5ec93f7955e639e0a4fb3b0ca5f29
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 15:14:30 -0300

    Configure routes modules user

--- app/Modules/User/Routes/index.ts
+++ app/Modules/User/Routes/index.ts
@@ -1,6 +1,15 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.resource('users', 'User/Controller/UserController').except(['edit', 'store', 'show'])
-  Route.get('/users/:id?', 'User/Controller/UserController.show').as('user.show')
+  Route.group(() => {
+    Route.resource('users', 'User/Controller/UserController').except([
+      'edit',
+      'create',
+      'show',
+      'store',
+    ])
+    Route.get('/users/:id?', 'User/Controller/UserController.show').as('user.show')
+  }).middleware('auth')
+
+  Route.post('/users', 'User/Controller/UserController.store').as('user.store')
 }).prefix('api')
