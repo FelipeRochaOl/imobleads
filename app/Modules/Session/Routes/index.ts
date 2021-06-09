98c5db2752726d8223fcf65ffc39b57e35df17f3
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 15:12:16 -0300

    update route /login

--- app/Modules/Session/Routes/index.ts
+++ app/Modules/Session/Routes/index.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.post('/login', 'Session/Controller/SessionController.create').as('login')
+  Route.post('/login', 'Session/Controller/SessionController.store').as('login')
 }).prefix('api')
