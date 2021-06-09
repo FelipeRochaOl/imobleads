9dfed840d8b89f803cb2ebf30cb5af7f256ee9c3
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 15:16:38 -0300

    Add midlewares to verify authentication

--- start/kernel.ts
+++ start/kernel.ts
@@ -1,3 +1,6 @@
 import Server from '@ioc:Adonis/Core/Server'
 Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
-Server.middleware.registerNamed({})
+Server.middleware.registerNamed({
+  auth: () => import('App/Modules/Session/Middleware/Auth'),
+  silentAuth: () => import('App/Modules/Session/Middleware/SilentAuth'),
+})
