c934e9e4ce7339442dd5759cdbdf172b63475b92
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-09 03:57:12 -0300

    create controller health

---
+++ app/Modules/Health/Controller/HealthController.ts
@@ -0,0 +1,9 @@
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class HealthController {
+  public async index({ response }: HttpContextContract) {
+    const report = await HealthCheck.getReport()
+    return report.healthy ? response.ok(report) : response.badRequest(report)
+  }
+}
