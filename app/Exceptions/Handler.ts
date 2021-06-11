89e13f91b8c3798ca55e7c6ecabb8d45982de55a
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-11 13:26:02 -0300

    Create handler to deal with errors

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -1,8 +1,21 @@
 import Logger from '@ioc:Adonis/Core/Logger'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { IResponseError } from 'App/Shared/Interfaces/IResponseError'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract) {
+    const errorMessage: IResponseError = {
+      message: 'Error processing your request',
+      success: false,
+    }
+
+    await super.handle(error, ctx)
+
+    return ctx.response.status(404).json(errorMessage)
   }
 }
