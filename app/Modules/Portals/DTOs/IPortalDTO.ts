9cad6d7bbd406c595c3f1c327506908be2a0f2f5
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-18 13:13:03 -0300

    Duplicate files

--- app/Modules/Portals/DTOs/IPortalDTO.ts
+++
@@ -1,9 +0,0 @@
-export interface IPortalDTO {
-  id?: number
-  name: string
-  id_user: string
-  limit_amount_ads?: number
-  limit_amount_featured_ads?: number
-  used_amount_ads?: number
-  used_amount_featured_ads?: number
-}
--- app/Modules/Portals/Routes/index.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-  Route.resource('portals', 'PortalController').apiOnly()
-})
-  .prefix('api')
-  .middleware(['api', 'auth'])
