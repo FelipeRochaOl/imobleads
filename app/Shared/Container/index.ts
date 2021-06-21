03bd9d0273f01c93db7f1a6fbe967dac251867b3
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-06-21 11:57:45 -0300

    Add new repositories to dependence injection

--- app/Shared/Container/index.ts
+++ app/Shared/Container/index.ts
@@ -6,8 +6,33 @@ import UserRepository from 'App/Modules/User/Repositories/UserRepository'
 import { IClientRepository } from 'App/Modules/Client/Interfaces/IClientRepository'
 import ClientRepository from 'App/Modules/Client/Repositories/ClientRepository'
 
+import { IImmobileRepository } from 'App/Modules/Immobile/Interfaces/IImmobileRepository'
+import ImmobileRepository from 'App/Modules/Immobile/Repositories/ImmobileRepository'
+
+import { IPortalRepository } from 'App/Modules/Portal/Interfaces/IPortalRepository'
+import PortalRepository from 'App/Modules/Portal/Repositories/PortalRepository'
+
+import { IContactRepository } from 'App/Modules/Client/Interfaces/IContactRepository'
+import ContactRepository from 'App/Modules/Client/Repositories/ContactRepository'
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+
 container.registerSingleton<IClientRepository>(
   'ClientRepository',
   ClientRepository
+)
+
+container.registerSingleton<IImmobileRepository>(
+  'ImmobileRepository',
+  ImmobileRepository
+)
+
+container.registerSingleton<IPortalRepository>(
+  'PortalRepository',
+  PortalRepository
+)
+
+container.registerSingleton<IContactRepository>(
+  'ContactRepository',
+  ContactRepository
 )
