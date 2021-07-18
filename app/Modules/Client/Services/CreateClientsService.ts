a77a4ae46e32b0ba5bbe3bd3ca5403712630363c
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-07-18 16:13:59 -0300

    New Config App

--- app/Modules/Client/Services/CreateClientsService.ts
+++ app/Modules/Client/Services/CreateClientsService.ts
@@ -12,13 +12,54 @@ export default class CreateClientsService {
     private clientRepository: IClientRepository
   ) {}
 
-  public async execute(data: IClientDTO): Promise<Client> {
-    const client = await this.clientRepository.create(data)
+  public async execute({
+    belongs_to_the_client_id,
+    cpf,
+    cnpj,
+    creci,
+    ...data
+  }: Partial<IClientDTO>): Promise<Client> {
+    if (!belongs_to_the_client_id) {
+      throw new Error('User is not authenticated')
+    }
+
+    await this.existsClientInDatabase('cpf', cpf)
+    await this.existsClientInDatabase('cnpj', cnpj)
+    await this.existsClientInDatabase('creci', creci)
+
+    const newData = Object.assign(
+      {},
+      {
+        belongs_to_the_client_id,
+        cpf,
+        cnpj,
+        creci,
+        ...data,
+      }
+    )
+    const client = await this.clientRepository.create(newData)
 
     if (!client) {
-      throw new Error('Unable to register the user')
+      throw new Error('Unable to register the client')
     }
 
     return client
+  }
+
+  protected async existsClientInDatabase(
+    column: string,
+    value: any
+  ): Promise<boolean | Error> {
+    if (!column || !value) return false
+
+    const exists = await this.clientRepository.findByUniqueIdentification({
+      [column]: value,
+    })
+
+    if (parseInt(exists.$extras.count) > 0) {
+      throw new Error(`Customer already registered with this ${column}`)
+    }
+
+    return false
   }
 }
