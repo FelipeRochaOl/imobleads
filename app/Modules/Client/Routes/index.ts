1d3a96bdfd61e8a22bb5f27e8cbb0936ad4fdb97
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-07-23 23:50:19 -0300

    Update routes

--- app/Modules/Client/Routes/index.ts
+++ app/Modules/Client/Routes/index.ts
@@ -2,8 +2,14 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.resource('clients', 'Client/Controllers/ClientController').apiOnly()
-  Route.resource('contacts', 'Client/Controllers/ContactController').apiOnly()
-  Route.resource('locations', 'Client/Controllers/LocationController').apiOnly()
+  Route.resource(
+    'clients/:client_id/contacts',
+    'Client/Controllers/ContactController'
+  ).apiOnly()
+  Route.resource(
+    'clients/:client_id/locations',
+    'Client/Controllers/LocationController'
+  ).apiOnly()
 })
   .prefix('api')
   .middleware(['auth', 'clientAuthorization'])
